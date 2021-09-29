9fe9c66e8ccc15ba73a75d98a4d38369b106fd67
nevdevmak <nevdevmak@gmail.com>
2021-09-29 14:48:04 +0200

    5. NetworkManager

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("I connected to a server!");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+        MyNetworkPlayer player =  conn.identity.GetComponent<MyNetworkPlayer>();
+        if(player == null) { return; }
+        player.SetDisplsyName($"Player{numPlayers}");
+
+       
+    }
+
+
+}
---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,16 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SyncVar]
+    [SerializeField]
+    private string displayName = "Missing Name";
+
+    public void SetDisplsyName(string newDisplayName)
+    {
+        displayName = newDisplayName;
+    }
+}
